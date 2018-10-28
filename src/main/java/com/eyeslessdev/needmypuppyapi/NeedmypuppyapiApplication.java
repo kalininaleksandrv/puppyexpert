7ef62627c050cb44b6c1f6740d94c2c9b1da9a0e
Калинин Александр <kalinin.aleksandr.v@gmail.com>
2018-10-28 16:53:58 +0300

    firs commit

---
+++ src/main/java/com/eyeslessdev/needmypuppyapi/NeedmypuppyapiApplication.java
@@ -0,0 +1,12 @@
+package com.eyeslessdev.needmypuppyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedmypuppyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedmypuppyapiApplication.class, args);
+	}
+}
