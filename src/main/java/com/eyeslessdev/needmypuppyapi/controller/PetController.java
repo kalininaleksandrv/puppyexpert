89542d709329783c65370f18cd8e45d7978d667b
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2018-10-28 19:31:21 +0300

    simple rest

---
+++ src/main/java/com/eyeslessdev/needmypuppyapi/controller/PetController.java
@@ -0,0 +1,62 @@
+package com.eyeslessdev.needmypuppyapi.controller;
+
+import com.eyeslessdev.needmypuppyapi.exceptions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("pet")
+public class PetController {
+
+        private int counter = 4;
+
+        private List<Map<String, String>> pets = new ArrayList<Map<String, String>>(){{
+            add(new HashMap<String, String>() {{put("id", "1"); put ("name", "1stname"); }});
+            add(new HashMap<String, String>() {{put("id", "2"); put ("name", "2ndname"); }});
+            add(new HashMap<String, String>() {{put("id", "3"); put ("name", "3rdname"); }});
+        }};
+
+        @GetMapping
+        public List<Map<String, String>> list (){
+            return pets;
+        }
+
+        @GetMapping("{id}")
+        private Map<String, String> getPetById(@PathVariable String id){
+            return getId(id);
+        }
+
+        @PostMapping
+        public Map<String,String> createNewPet (@RequestBody Map<String, String> pet){
+
+            pet.put("id", String.valueOf(counter++));
+            pets.add(pet);
+            return pet;
+        }
+
+        @PutMapping("{id}")
+        public Map<String, String> updatePetById(@PathVariable String id, @RequestBody Map<String, String> pet){
+
+            Map<String, String> petFromDb = getId(id);
+            petFromDb.putAll(pet);
+            petFromDb.put("id", id);
+
+            return petFromDb;
+        }
+
+        @DeleteMapping ("{id}")
+        public void deleatPetById (@PathVariable String id){
+            pets.remove(getPetById(id));
+        }
+
+        private Map<String, String> getId(@PathVariable String id) {
+            return pets.stream()
+                    .filter(pet -> pet.get("id").equals(id))
+                    .findFirst()
+                    .orElseThrow(NotFoundException::new);
+        }
+}
