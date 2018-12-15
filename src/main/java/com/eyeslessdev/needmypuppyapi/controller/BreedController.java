199b60ee9795ba74fe3eb7ddad36eb6a86089f8b
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2018-12-16 01:42:24 +0300

    add favoriting

--- src/main/java/com/eyeslessdev/needmypuppyapi/controller/BreedController.java
+++ src/main/java/com/eyeslessdev/needmypuppyapi/controller/BreedController.java
@@ -1,36 +1,39 @@
 package com.eyeslessdev.needmypuppyapi.controller;
 
 import com.eyeslessdev.needmypuppyapi.entity.Breed;
-import com.eyeslessdev.needmypuppyapi.repositories.BreedRepo;
 import com.eyeslessdev.needmypuppyapi.service.BreedService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Comparator;
 import java.util.List;
 
 @RestController
 @CrossOrigin
+@RequestMapping("breeds")
 public class BreedController {
-    
+
     @Autowired
     private BreedService breedService;
 
     @GetMapping
-    @RequestMapping("breeds")
     public List<Breed> getAllBreedsOrderedByTitle (){
 
         return breedService.getAllBreedsOrderedByTitle();
     }
 
+    @CrossOrigin
     @GetMapping
-    @RequestMapping("breedsbyid")
+    @RequestMapping("byid")
     public List<Breed> getAllBreedsById (){
 
         return breedService.getAllBreedsOrderedById();
+    }
+
+    @CrossOrigin
+    @GetMapping("faved/{id}")
+    public ResponseEntity<Object> faveBreed(@PathVariable long id) {
+        return breedService.faveBreedById(id);
     }
 
 }
--- src/main/java/com/eyeslessdev/needmypuppyapi/service/BreedService.java
+++ src/main/java/com/eyeslessdev/needmypuppyapi/service/BreedService.java
@@ -3,10 +3,12 @@ package com.eyeslessdev.needmypuppyapi.service;
 import com.eyeslessdev.needmypuppyapi.entity.Breed;
 import com.eyeslessdev.needmypuppyapi.repositories.BreedRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BreedService {
@@ -20,15 +22,29 @@ public class BreedService {
     }
 
     public List<Breed> getAllBreedsOrderedById(){
-
         List<Breed> rawlist = breedRepo.findAll();
         rawlist.sort(Comparator.comparing(Breed::get_id));
         return rawlist;
     }
 
     public List<Breed> getAllBreedsOrderedByTitle(){
-
         return breedRepo.findAllByOrderByTitle();
     }
 
+    public ResponseEntity<Object> faveBreedById(long id) {
+
+        Optional<Breed> breedOptional = breedRepo.findById(id);
+
+        if (!breedOptional.isPresent())
+            return ResponseEntity.notFound().build();
+        else {
+        Breed breed = breedOptional.get();
+        breed.setFavorite(increasefav(breed.getFavorite()));
+        breedRepo.save(breed);
+        return ResponseEntity.noContent().build();}
+    }
+
+    private int increasefav(int favorite) {
+        return ++favorite;
+    }
 }
