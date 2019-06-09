38e67ed28ff0fc177dd6688100c897bc78314ddd
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2019-06-09 16:57:27 +0300

    big refactoring half way

--- src/main/java/com/eyeslessdev/needmypuppyapi/entity/BreedRequestFactory.java
+++ src/main/java/com/eyeslessdev/needmypuppyapi/entity/BreedRequestFactory.java
@@ -2,35 +2,75 @@ package com.eyeslessdev.needmypuppyapi.entity;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class BreedRequestFactory {
 
-    public BreedRequest getBreedReqwest (Map<String, Integer> brpselect, Map<String, Integer> brpconstraint, Map<String, String> brpexterier){
+    public BreedRequest getBreedRequest (Map<String, String> allparam){
+
+
+        Map<String, Integer> outcomemap = new HashMap<>();
+
+        Map<String, String> outcomemapstr = new HashMap<>();
+
+        for (Map.Entry<String, String> item : allparam.entrySet()) {
+            //check if created map contains fetched value
+            for (String compareditem : getSelectorArray()) {
+                if (item.getKey().contains(compareditem)) {
+                    outcomemap.put(item.getKey(), Integer.parseInt(item.getValue()));
+                } else outcomemapstr.put(item.getKey(), item.getValue());
+            }
+        }
 
         return new BreedRequest(
-                brpselect.get("time"),
-                brpselect.get("exp"),
-                brpselect.get("age"),
-                brpselect.get("athlet"),
-                brpselect.get("cyno"),
-                brpselect.get("walk"),
-                brpselect.get("fam"),
-                brpselect.get("grum"),
-                brpconstraint.get("foragility"),
-                brpconstraint.get("forchild"),
-                brpconstraint.get("forcompany"),
-                brpconstraint.get("forguardter"),
-                brpconstraint.get("forhunt"),
-                brpconstraint.get("forobidience"),
-                brpconstraint.get("forruning"),
-                brpconstraint.get("forzks"),
-                brpexterier.get("hairsize"),
-                brpexterier.get("blackorwhite"),
-                brpconstraint.get("sizeconstraintmin"),
-                brpconstraint.get("sizeconstraintmax"),
-                brpexterier.get("rare"));
+                outcomemap.get("time"),
+                outcomemap.get("exp"),
+                outcomemap.get("age"),
+                outcomemap.get("athlet"),
+                outcomemap.get("cyno"),
+                outcomemap.get("walk"),
+                outcomemap.get("fam"),
+                outcomemap.get("grum"),
+                outcomemap.get("foragility"),
+                outcomemap.get("forchild"),
+                outcomemap.get("forcompany"),
+                outcomemap.get("forguardter"),
+                outcomemap.get("forhunt"),
+                outcomemap.get("forobidience"),
+                outcomemap.get("forruning"),
+                outcomemap.get("forzks"),
+                outcomemapstr.get("hairsize"),
+                outcomemapstr.get("blackorwhite"),
+                outcomemap.get("sizeconstraintmin"),
+                outcomemap.get("sizeconstraintmax"),
+                outcomemapstr.get("rare"));
 
+    }
+
+
+
+    private String[] getSelectorArray () {
+
+        return new String[]{"time",
+                "exp",
+                "age",
+                "athlet",
+                "cyno",
+                "walk",
+                "fam",
+                "grum",
+                "foragility",
+                "forchild",
+                "forcompany",
+                "forguardter",
+                "forhunt",
+                "forobidience",
+                "forruning",
+                "forzks",
+                "sizeconstraintmin",
+                "sizeconstraintmax"
+        };
     }
 }
