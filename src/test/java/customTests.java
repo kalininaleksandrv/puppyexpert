55b7bab44c7ce275fc11a7af4bdcd0ff938d6302
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2019-09-17 18:30:47 +0300

    add simple unit test

---
+++ src/test/java/customTests.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.*;
+
+class customTests {
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("@BeforeAll - executes once before all test methods in this class");
+    }
+
+    @BeforeEach
+    void init() {
+        System.out.println("@BeforeEach - executes before each test method in this class");
+    }
+
+    @DisplayName("Single test successful")
+    @Test
+    void testSingleSuccessTest() {
+        System.out.println("Success");
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void testShowSomething() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("@AfterEach - executed after each test method.");
+    }
+
+    @AfterAll
+    static void done() {
+        System.out.println("@AfterAll - executed after all test methods.");
+    }
+}
